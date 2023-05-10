b9f2333b254c827a7431f7f0db58aa4a5e410e59
Sumit Jha <130956050+SumitJha20@users.noreply.github.com>
2023-05-10 17:03:21 +0530

    Update WeatherController.java

--- src/main/java/com/example/controller/WeatherController.java
+++ src/main/java/com/example/controller/WeatherController.java
@@ -5,14 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
